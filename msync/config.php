89b4ce31eea69e3b33856173b093af034f32e111
Rude Boy <autopsy@protonmail.com>
2017-01-25 06:02:58 -0500

    removed useless files

--- msync/config.php
+++ msync/config.php
@@ -9,3 +9,5 @@ define("DB_PASSWORD", "As3mp@:kw");
 
 define("DB_DATABASE", "ghc");
 
+//working with local server before pushing database online
+//let me know if u will need the online database configuration, kwaku.
